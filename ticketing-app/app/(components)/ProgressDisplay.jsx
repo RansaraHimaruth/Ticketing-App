8d4ecaefe079c5ae1f8ce274a6ee4c6e55d2d7e8
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-06-07 23:16:17 +0530

    creating the basic structure of the card ticket component

---
+++ ticketing-app/app/(components)/ProgressDisplay.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProgressDisplay = () => {
+  return (
+    <div>
+      <div className="w-full bg-gray-200 rounded-full h-2.5">
+        <div
+          className="bg-blue-600 h-2.5 rounded-full"
+          style={{ width: "75%" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressDisplay;
--- ticketing-app/app/page.jsx
+++ ticketing-app/app/page.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard=()=> {
-  return <div><TicketCard/></div>;
-}
+const Dashboard = () => {
+  return (
+    <div className="p-5">
+      <div className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
