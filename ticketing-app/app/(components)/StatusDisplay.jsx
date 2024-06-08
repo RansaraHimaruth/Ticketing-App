5d2d69b311db823af5353f9008ee50abf93f6c22
RansaraHimaruth <ransara.20221894@iit.ac.lk>
2024-06-08 16:20:08 +0530

    formated the data in ticket
    *get the real ticket data from the database

--- ticketing-app/app/(components)/StatusDisplay.jsx
+++ ticketing-app/app/(components)/StatusDisplay.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
-const StatusDisplay = () => {
+const StatusDisplay = ({status}) => {
+
+  const getColor = (status) => {
+    let color = "bg-slate-700"
+    switch (status.toLowerCase()) {
+      case "completed":
+        color = "bg-green-200"
+        return color
+      case "in progress":
+        color = "bg-yellow-200"
+        return color
+      case "not started":
+        color = "bg-red-300"
+        return color
+        
+  }
+  return color;
+}
+
   return (
     <div>
-      <span className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200'>
-        done
+      <span className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(status)}`}>
+        {status}
       </span>
     </div>
   )
